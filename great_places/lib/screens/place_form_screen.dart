6484d52578b0193254e5485bbb625dd89fe1429e
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-28 14:03:45 -0300

    Pagina do formulário criada

--- great_places/lib/screens/place_form_screen.dart
+++ great_places/lib/screens/place_form_screen.dart
@@ -1,21 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
-  const PlaceFormScreen({super.key});
+  const PlaceFormScreen({Key? key}) : super(key: key);
 
   @override
   State<PlaceFormScreen> createState() => _PlaceFormScreenState();
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('New place'),
+        title: const Text('Novo Lugar'),
       ),
-      body: const Center(
-        child: Text('Form!!'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              foregroundColor: Colors.black,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            onPressed: _submitForm,
+          ),
+        ],
       ),
     );
   }
