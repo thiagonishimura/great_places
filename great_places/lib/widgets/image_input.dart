ed23b9cb0a3c70e5dcdffaa5b4ef6869cd8f746e
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-04-28 16:49:43 -0300

    Acessando a camera

--- great_places/lib/widgets/image_input.dart
+++ great_places/lib/widgets/image_input.dart
@@ -1,13 +1,29 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({super.key});
+  const ImageInput({Key? key}) : super(key: key);
 
   @override
   State<ImageInput> createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    XFile imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    ) as XFile;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -16,20 +32,23 @@ class _ImageInputState extends State<ImageInput> {
           width: 180,
           height: 100,
           decoration: BoxDecoration(
-            border: Border.all(
-              width: 1,
-              color: Colors.grey,
-            ),
+            border: Border.all(width: 1, color: Colors.grey),
           ),
           alignment: Alignment.center,
-          child: const Text('Nenuma imagem'),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : const Text('Nenhuma imagem!'),
         ),
         const SizedBox(width: 10),
         Expanded(
           child: TextButton.icon(
             icon: const Icon(Icons.camera),
             label: const Text('Tirar Foto'),
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
